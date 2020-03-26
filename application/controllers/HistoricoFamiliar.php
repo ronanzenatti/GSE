6323d3256069d7867cc7e92190eb883b4a3f9c92
Jessica Cunha <jessic4cunha@gmail.com>
2020-03-26 18:42:23 -0300

    delete de controllers não usados

--- application/controllers/HistoricoFamiliar.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class HistoricoFamiliar extends CI_Controller
-{
-}
--- application/controllers/Processos.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Processos extends CI_Controller
-{
-}
