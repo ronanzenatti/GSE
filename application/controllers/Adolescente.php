4bf37734bffe59d7c28d9955d75a6e24ffdfe494
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-03-30 16:28:16 -0300

    Controller grupo_familiar e CRUD composição_familiar

--- application/controllers/Adolescente.php
+++ application/controllers/Adolescente.php
@@ -82,7 +82,7 @@ class Adolescente extends CI_Controller
 
 			$btns = "<a href='" . base_url('adolescente/alterar/' . $obj->id_adolescente) . "' class='btn btn-warning btn-sm'><i class='fa fa-pencil' aria-hidden='true'></i></a> ";
 			$btns .= "<button type='button' onclick='deletarRegistro(\"adolescente\", " . $obj->id_adolescente . ", \"tableAdolescente\")' class='btn btn-danger btn-sm'><i class='fa fa-trash-o' aria-hidden='true'></i></button>  ";
-			$btns .= "<a href='" . base_url('ComposicaoFamiliar/adolescente/' . $obj->id_adolescente) . "' class='btn btn-info btn-sm' class='btn btn-info btn-sm' data-toggle='tooltip' data-placement='top' title='Composição Familiar' data-original-title='Composição Familiar'><i class='fa fa-users' aria-hidden='true'></i></a> ";
+			$btns .= "<a href='" . base_url('GrupoFamiliar/save/' . $obj->id_adolescente) . "' class='btn btn-info btn-sm' class='btn btn-info btn-sm' data-toggle='tooltip' data-placement='top' title='Composição Familiar' data-original-title='Composição Familiar'><i class='fa fa-users' aria-hidden='true'></i></a> ";
 			$btns .= "<a target='_blank' href='" . base_url('adolescente/gerar/' . $obj->id_adolescente . '/' . $_SESSION['entidade_id']) . "' class='btn btn-primary btn-sm'><i class='fa fa-file-pdf-o' aria-hidden='true'></i></a> ";
 
 			$row[] = $btns;
